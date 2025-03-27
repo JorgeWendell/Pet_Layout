2fff6f312d274e55ac12dd0859c47f4e1eca2c4b
JorgeWendell <jorge.wendell@adelbr.tech>
2025-03-27 09:51:49 -0300

    fix digitacao

--- src/app/_components/about.tsx
+++ src/app/_components/about.tsx
@@ -58,7 +58,7 @@ export default function About() {
                     className="bg-green-500 text-white flex items-center justify-center w-fit gap-4 px-4 py-2 rounded-md"
                     >
                         <WhatsappLogo className="w-5 h-5 text-white" />
-                        Contato via WhatApp
+                        Contato via WhatsApp
                     </a>
                     <a href=""
                     className=" flex items-center justify-center w-fit gap-4 px-4 py-2 rounded-md"
